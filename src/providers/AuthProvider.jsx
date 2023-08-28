37b15a4c4e5503d6752bccca3dd85f2815a930c7
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-08-28 20:16:27 +0600

    firebase added with configuration hidden in .env file

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,9 +1,14 @@
 import { createContext, useState } from "react";
+import { getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+
+
 
     const authInfo = {
         user,
