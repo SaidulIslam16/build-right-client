81c6c2598f2b528b28aa0d221c5809da069d780a
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-09-05 17:47:28 +0600

    course cards added

--- src/Pages/Home/PopularCourses/PopularCourses.jsx
+++ src/Pages/Home/PopularCourses/PopularCourses.jsx
@@ -1,6 +1,6 @@
-import { data } from "autoprefixer";
 import { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import CourseCard from "../../../Shared/CourseCard/CourseCard";
 
 const PopularCourses = () => {
 
@@ -9,15 +9,21 @@ const PopularCourses = () => {
     useEffect(() => {
         fetch('course.json')
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setCourses(data))
     }, [])
 
 
     return (
 
-        <div>
+        <section className="my-10">
             <SectionTitle heading='Popular Courses' subHeading="Explore Our"></SectionTitle>
-        </div>
+            <div className="grid md:grid-cols-3 gap-5">
+                {
+                    courses.map((course, i) => <CourseCard key={i} course={course}></CourseCard>)
+                }
+            </div>
+
+        </section>
     );
 };
 
---
+++ src/Shared/CourseCard/CourseCard.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+
+const CourseCard = ({ course }) => {
+    const { name, image, instructor_name, price, available_seats } = course;
+    return (
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure><img src={image} alt="course images" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <p>Instructor name: <span className="font-bold">{instructor_name}</span></p>
+                <p>Available seats: <span className="font-bold">{available_seats}</span></p>
+                <div className="card-actions justify-end mt-4">
+                    <p className="text-xl font-bold">${price}</p>
+                    <Link className="btn btn-primary">Buy Now</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CourseCard;
