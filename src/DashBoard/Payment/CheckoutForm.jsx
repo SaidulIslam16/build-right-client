3d2d8f3f5f4e1585652f878dc52396622e3b5e05
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-09-07 20:28:43 +0600

    added create payment method for stripe and showing error in client side

--- src/DashBoard/Payment/CheckoutForm.jsx
+++ src/DashBoard/Payment/CheckoutForm.jsx
@@ -1,9 +1,11 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const CheckoutForm = () => {
 
     const stripe = useStripe();
     const elements = useElements();
+    const [cardError, setCardError] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -22,30 +24,48 @@ const CheckoutForm = () => {
         if (card == null) {
             return;
         }
+
+        // Use your card Element with other Stripe.js APIs
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+
+        if (error) {
+            console.log('[error]', error);
+            setCardError(error.message);
+        }
+        else {
+            console.log('[PaymentMethod]', paymentMethod);
+        }
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button type="submit" className="btn btn-warning mt-4" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
+                    }}
+                />
+                <button type="submit" className="btn btn-warning mt-4" disabled={!stripe}>
+                    Pay
+                </button>
+            </form>
+
+            <div>{cardError && <p className="text-red-600 mt-4">{cardError}</p>}</div>
+        </>
     );
 };
 
